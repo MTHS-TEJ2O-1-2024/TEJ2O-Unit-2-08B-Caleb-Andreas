fe7efa1dbcbf98c7ced9eb08a48727cc271a793e
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-03 11:28:34 -0400

    Made a code that scrolls through RGB LED colours

--- main.ts
+++ main.ts
@@ -1,8 +1,65 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Oct 2024
+ * This program scrolls through RGB LED colours
 */
 
-basic.showString('Hello, World!')
+// Happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// RGB LED scroll through 7 colours when A button pressed.
+input.onButtonPressed(Button.A, function() {
+    //Red
+    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("Red")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    //Blue
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.showString("Blue")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    //Green
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.showString("Green")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    //Magenta
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Magenta")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    //Yellow
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.showString("Yellow")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    //Cyan
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("Cyan")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    //White
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString("White")
+    basic.pause(1000)
+    basic.clearScreen()
+
+    // Happy face
+    basic.showIcon(IconNames.Happy)
+})
